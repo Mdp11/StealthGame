074f21904a3e7a34c7cabb010b9e747a5fae6538
Mattia De Prisco <mattia.dipi@gmail.com>
2021-05-14 14:44:49 +0200

    Add Objective Actor

---
+++ Source/FPSGame/Public/FPSObjectiveActor.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/Actor.h"
+#include "FPSObjectiveActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class FPSGAME_API AFPSObjectiveActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFPSObjectiveActor();
+
+protected:
+	UPROPERTY(EditAnywhere, Category="Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(EditAnywhere, Category="Components")
+	USphereComponent* SphereComp;
+
+	UPROPERTY(EditDefaultsOnly, Category="Effects")
+	UParticleSystem* PickupFX;
+
+	virtual void BeginPlay() override;
+	
+	void PlayEffects() const;
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+};
